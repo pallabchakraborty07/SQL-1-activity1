c3ca3666c762c7adb387cd7779aa8cdca0113c40
pallabchakraborty07 <chpallab07@gmail.com>
2024-09-21 23:27:11 +0530

    Add files via upload

---
+++ activity3.sql
@@ -0,0 +1,13 @@
+CREATE TABLE SALESMAN(
+salesman_id TEXT,
+name TEXT,
+city TEXT,
+comission REAL
+);
+INSERT INTO SALESMAN(salesman_id,name,city,comission) VALUES
+("5001","PALLAB","LONDON",0.15),
+("5002","JHON","PARIS",0.32),
+("5005","RAHUL","HIMALAY",0.12),
+("5006","PAUL","ROME",0.35),
+("5008","ROHIT","RAJASTHAN",0.24);
+SELECT * FROM SALESMAN;
